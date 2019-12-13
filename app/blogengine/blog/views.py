d749a8d21d5f3517c565609ff43f86b2b78bfd34
Neal O <nealorven@gmail.com>
2019-12-13 14:30:35 +0600

    Update

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -7,17 +7,43 @@ from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 
+from django.db.models import Q
+
 from .utils import *
 from .models import Post, Tag
 from .forms import PostForm, TagForm
 
 
 def posts_list(request):
-    posts = Post.objects.all()
-    paginator = Paginator(posts, 1)
+    search_query = request.GET.get('search', '')
+
+    if search_query:
+        posts = Post.objects.filter(Q(title__icontains=search_query) | Q(body__icontains=search_query))
+    else:
+        posts = Post.objects.all()
+
+    paginator = Paginator(posts, 3)
     page_number = request.GET.get('page', 1)
     page = paginator.get_page(page_number)
-    return render(request, 'blog/index.html', context={'page_object': page})
+    is_paginated = page.has_other_pages()
+
+    if page.has_previous():
+        prev_url = '?page={}'.format(page.previous_page_number())
+    else:
+        prev_url = ''
+
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url = ''
+
+    context = {
+        'page_object': page,
+        'is_paginated': is_paginated,
+        'prev_url': prev_url,
+        'next_url': next_url
+    }
+    return render(request, 'blog/index.html', context=context)
 
 
 class PostCreate(LoginRequiredMixin, ObjectCreateMixin, View):
